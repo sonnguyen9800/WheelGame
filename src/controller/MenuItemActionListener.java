419fed71c6763d9300473af4de1f70ec02ea6770
SonNguyen9800 <sonnguyen9800@gmail.com>
2019-05-29 19:21:46 +1000

    Last Edit

--- src/controller/MenuItemActionListener.java
+++ src/controller/MenuItemActionListener.java
@@ -1,28 +1,21 @@
 package controller;
 
 import view.Dialog.PlayerNewDialog;
-import view.GameEngineCallbackGUI;
 
 import javax.swing.*;
-import javax.swing.event.MenuEvent;
-import javax.swing.event.MenuListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+
 
 public class MenuItemActionListener implements ActionListener {
-    JFrame frame;
-    PlayerNewDialog playerNewDialog;
 
     public MenuItemActionListener(JFrame jFrame) {
         super();
-        this.frame = jFrame;
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-        playerNewDialog = new PlayerNewDialog();
+        PlayerNewDialog playerNewDialog = new PlayerNewDialog();
 
     }
 }
